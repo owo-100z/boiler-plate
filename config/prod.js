2515bdb0d1f5aaaab4e75d9d50f8f370234ff579
100z <100zowo@gmail.com>
2023-06-22 15:30:24 +0900

    config update

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,9 @@ module.exports = {
     db_name: process.env.DB_NAME,
     db_user: process.env.DB_USER,
     db_pass: process.env.DB_PASS,
-    t_option: process.env.TOKEN_OPTION,
+    t_option: {
+        algorithm: 'HS256',
+        expiresIn: '1m',
+        issuer: 'issure'
+    },
 }
